d9321358d7405e452544ec761de7a66990945f12
John Barson <jkbarson@gmail.com>
2017-12-05 22:03:58 -0500

    comment out code used for mongoose.  Check usage for MySQL

--- server.js
+++ server.js
@@ -26,29 +26,31 @@ app.use(express.static("public"));
 // if else statement to use localhost if not being hoseted on Herko
 //  Seemed helpful for development without having to switch code repeatedly
 // credit to davesrose for the if/else setup
-if(process.env.NODE_ENV == 'production'){
 
-  // hosted MySQl
-}
-else{
-  // local MySQL
-}
-
-const db = mongoose.connection;
-
-// Show any Mongoose errors
-db.on('error', function(err) {
-  console.log('MySQL Error: ', err);
-});
-
-// Once logged in to the db through mongoose, log a success message
-db.once('open', function() {
-  console.log('MySQL connection successful.');
-});
-
-// Import Routes/Controller
-const router = require('./controllers/controller.js');
-app.use('/', router);
+// Below code was used for Mongoose.  This may not be correct for sequelize.
+// if(process.env.NODE_ENV == 'production'){
+//
+//   // hosted MySQl
+// }
+// else{
+//   // local MySQL
+// }
+//
+// const db = mongoose.connection;
+//
+// // Show any Mongoose errors
+// db.on('error', function(err) {
+//   console.log('MySQL Error: ', err);
+// });
+//
+// // Once logged in to the db through mongoose, log a success message
+// db.once('open', function() {
+//   console.log('MySQL connection successful.');
+// });
+//
+// // Import Routes/Controller
+// const router = require('./controllers/controller.js');
+// app.use('/', router);
 
 
 // Launch App
