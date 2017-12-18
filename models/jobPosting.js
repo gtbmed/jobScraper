b19b604b09ac12bf6c7f329a3c09dd537d175cf1
John Barson <jkbarson@gmail.com>
2017-12-17 21:33:51 -0500

    finish jobPosting model

--- models/jobPosting.js
+++ models/jobPosting.js
@@ -1,3 +1,25 @@
 module.exports = function(sequelize, DataTypes) {
-  const JobPosting = sequelize.define("JobPosting")
-}
+  const JobPosting = sequelize.define("JobPosting", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(val) {
+      this.setDataValue('title', val.toUpperCase());
+      }
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    saved: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    applied : {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+};
